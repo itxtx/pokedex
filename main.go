e75b5ff20cae77b1092112e3b90c3323c8781c57
itxtx <68209227+itxtx@users.noreply.github.com>
2024-08-26 09:01:39 +0200

    add help, exit

--- main.go
+++ main.go
@@ -1,7 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type cliCmd struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func Scanner(s *bufio.Scanner, commands map[string]cliCmd) {
+	for s.Scan() {
+		text := s.Text()
+		if cmd, found := commands[text]; found {
+			if err := cmd.callback(); err != nil {
+				fmt.Println("Error:", err)
+			}
+		} else {
+			fmt.Println("Unknown command:", text)
+		}
+	}
+	if err := s.Err(); err != nil {
+		fmt.Println("Error reading input:", err)
+	}
+}
+
+func commandHelp() error {
+	fmt.Println("Available commands:")
+	fmt.Println("help - displays help message")
+	fmt.Println("exit - exit pokedex")
+	return nil
+}
+
+func commandExit() error {
+	fmt.Println("Exiting...")
+	os.Exit(0)
+	return nil
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	commands := map[string]cliCmd{
+		"help": {
+			name:        "help",
+			description: "displays help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "exit pokedex",
+			callback:    commandExit,
+		},
+	}
+
+	fmt.Println("Pokedex > ")
+
+	// Create a new scanner for standard input
+	scanner := bufio.NewScanner(os.Stdin)
+
+	// Pass the scanner and commands map to the scanner function
+	Scanner(scanner, commands)
 }
